89028cc22db5a0b18078cfa4f68a15a18b62e05d
sergeikostiaev <skprod.info@gmail.com>
2024-10-19 18:08:38 +0400

    Change DB

--- server/config/db.js
+++ server/config/db.js
@@ -5,4 +5,16 @@ const sequelize = new Sequelize('callme_db', 'callme_user', 'yourpassword', {
     dialect: 'postgres'
 });
 
+// Проверка подключения
+const connectToDatabase = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Соединение с базой данных успешно установлено.');
+    } catch (error) {
+        console.error('Не удалось подключиться к базе данных:', error);
+    }
+};
+
+connectToDatabase();
+
 module.exports = sequelize;
